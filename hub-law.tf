c83f419e6ba0ba8ba070b7e31b4c076711a7a856
Markus Kraus <markus.kraus@gmail.com>
2024-05-28 22:45:23 +0200

    azurerm_network_connection_monitor

---
+++ hub-law.tf
@@ -0,0 +1,6 @@
+resource "azurerm_log_analytics_workspace" "law_platform" {
+  name                = "law-platform"
+  location            = azurerm_resource_group.rg_hub.location
+  resource_group_name = azurerm_resource_group.rg_hub.name
+  sku                 = "PerGB2018"
+}
