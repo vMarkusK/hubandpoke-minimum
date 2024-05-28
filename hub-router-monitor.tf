1643835c37a0af94e3abf58ad336d87621fb9361
Markus Kraus <markus.kraus@gmail.com>
2024-05-28 22:47:52 +0200

    tags

--- hub-router-monitor.tf
+++ hub-router-monitor.tf
@@ -2,6 +2,7 @@ resource "azurerm_network_watcher" "networkwatcher_hub" {
   name                = "network-watcher-hub"
   location            = azurerm_resource_group.rg_hub.location
   resource_group_name = azurerm_resource_group.rg_hub.name
+  tags                = var.tags
 }
 
 resource "azurerm_virtual_machine_extension" "ubunturoutervm_networkwatcher" {
@@ -11,12 +12,14 @@ resource "azurerm_virtual_machine_extension" "ubunturoutervm_networkwatcher" {
   type                       = "NetworkWatcherAgentLinux"
   type_handler_version       = "1.4"
   auto_upgrade_minor_version = true
+  tags                       = var.tags
 }
 
 resource "azurerm_network_connection_monitor" "ubunturoutervm_monitor_ubuntucom" {
   name               = "${var.hub_router_hostname}-ubuntucom"
   network_watcher_id = azurerm_network_watcher.networkwatcher_hub.id
   location           = azurerm_network_watcher.networkwatcher_hub.location
+  tags               = var.tags
 
   endpoint {
     name               = "source"
