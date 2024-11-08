48e8b932b923737ec8a958fd9f1ec63aca41fb63
Markus Kraus <markus.kraus@gmail.com>
2024-11-08 19:28:00 +0000

    new azurerm

--- hub-flow.tf
+++ hub-flow.tf
@@ -11,7 +11,7 @@ resource "azurerm_storage_account" "flownsg" {
   account_tier              = "Standard"
   account_kind              = "StorageV2"
   account_replication_type  = "LRS"
-  enable_https_traffic_only = true
+  https_traffic_only_enabled = true
   min_tls_version           = "TLS1_2"
 
   tags = var.tags
