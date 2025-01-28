a72407b54b935046e2e81cbba7176f79dd230e0f
Markus Kraus <markus.kraus@gmail.com>
2025-01-28 20:34:48 +0000

    minor reference

--- modules/spokedefault/main.tf
+++ modules/spokedefault/main.tf
@@ -7,9 +7,9 @@ resource "azurerm_resource_group" "rg-spoke" {
 # VNet
 resource "azurerm_virtual_network" "vnet-spoke" {
   name                = var.vnet_name
-  location            = var.location
-  address_space       = ["${var.address_space}"]
+  location            = azurerm_resource_group.rg-spoke.location
   resource_group_name = azurerm_resource_group.rg-spoke.name
+  address_space       = ["${var.address_space}"]
   dns_servers         = var.dns_servers
   tags                = var.tags
 }
